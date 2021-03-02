1c7bf62d842c2c627a7e9922840cc5fd3fe112d5
Imperial Owl <yogazacty@gmail.com>
2021-03-02 21:29:35 +0700

    Add support video story (#1)
    * Add support video story
    
    * fix: update data

--- dispatcher.js
+++ dispatcher.js
@@ -1,6 +1,12 @@
 require('dotenv').config()
 
-const {loadData, storeData, downloadImage, upsertDirectory} = require('./util')
+const {
+    loadData,
+    storeData,
+    updateData,
+    downloadImage,
+    upsertDirectory
+} = require('./util')
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TOKEN_TELEGRAM;
 const bot = new TelegramBot(token);
@@ -8,23 +14,28 @@ const chatID = process.env.CHAT_ID;
 
 const metadataStory = loadData("./metadata-story.json");
 
-// let updatedMetadataStory = [];
+metadataStory
+    .map(story => {
+        if (story.send) return
+        if (story.mediaType == 1) {
+            bot.sendPhoto(chatID, story.url)
+                .then((data) => {
+                    console.log("Success send " + story.id + " to " + data.chat.title + " (" + data.chat.id + ")")
+                    updateData("./metadata-story.json", story.id, true)
+                }).catch((error) => {
+                    console.log("failed to send: " + story.id)
+                    console.error(error)
+                })
+        } else if (story.mediaType == 2) {
+            bot.sendVideo(chatID, story.url)
+                .then((data) => {
+                    console.log("Success send " + story.id + " to " + data.chat.title + " (" + data.chat.id + ")")
+                    updateData("./metadata-story.json", story.id, true)
+                }).catch((error) => {
+                    console.log("failed to send: " + story.id)
+                    console.error(error)
+                })
+        }
+    })
 
-let updatedMetadataStory = metadataStory
-.map(story => {
-    if(!story.send){
-        bot.sendPhoto(chatID, story.url)
-        .then(() => {
-            console.log("Send Success"+story.id)
-            story.send = true;
-            return story
-        })
-        .catch(() => {
-            console.log("failed to send"+story.id)
-            return story
-        })
-    }
-    return story
-})
-
-    storeData("./metadata-story.json", updatedMetadataStory)
+// storeData("./metadata-story.json", updatedMetadataStory)
