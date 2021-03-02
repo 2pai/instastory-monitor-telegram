1c9e70a0ee2f49d80506af586590941abaec66ef
Yogs <yogazacty@gmail.com>
2021-03-02 19:50:20 +0700

    Add support video story

--- dispatcher.js
+++ dispatcher.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const {loadData, storeData, downloadImage, upsertDirectory} = require('./util')
+const { loadData, storeData, downloadImage, upsertDirectory } = require('./util')
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TOKEN_TELEGRAM;
 const bot = new TelegramBot(token);
@@ -11,20 +11,35 @@ const metadataStory = loadData("./metadata-story.json");
 // let updatedMetadataStory = [];
 
 let updatedMetadataStory = metadataStory
-.map(story => {
-    if(!story.send){
-        bot.sendPhoto(chatID, story.url)
-        .then(() => {
-            console.log("Send Success"+story.id)
-            story.send = true;
-            return story
-        })
-        .catch(() => {
-            console.log("failed to send"+story.id)
-            return story
-        })
-    }
-    return story
-})
+    .map(story => {
+        try {
+            if (story.send) return story
+            if (story.mediaType == 1) {
 
-    storeData("./metadata-story.json", updatedMetadataStory)
+                const sendMedia = bot.sendPhoto(chatID, story.url)
+                if (sendMedia.ok) {
+                    console.log("Send Success: " + story.id)
+                    story.send = true;
+                    story.msgId = data.message_id
+                    return story
+                }
+
+            } else if (story.mediaType == 2) {
+
+                const sendMedia = bot.sendVideo(chatID, story.url)
+                if (sendMedia.ok) {
+                    console.log("Send Success: " + story.id)
+                    story.send = true;
+                    story.msgId = data.message_id
+                    return story
+                }
+
+            }
+        } catch (error) {
+            console.log("failed to send: " + story.id)
+            console.error(error)
+        }
+        return story
+    })
+
+storeData("./metadata-story.json", updatedMetadataStory)
