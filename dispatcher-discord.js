b7f1c21c6924931d767455afee439e447109d438
Iqbal Syamil Ayasy <iqbal@pintu.co.id>
2021-03-21 16:49:49 +0700

    dispatcher-discord: init support for discord webhook dispatcher
    Signed-off-by: Iqbal Syamil Ayasy <iqbal@pintu.co.id>

---
+++ dispatcher-discord.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+
+const {
+    loadData,
+    updateData,
+} = require('./util')
+const webhook = require("webhook-discord")
+const webhookURL = process.env.DC_WEBHOOK_URL;
+const Hook = new webhook.Webhook(webhookURL)
+
+const metadataStory = loadData("./metadata-story.json");
+
+metadataStory
+    .map(story => {
+        if (story.send) return
+        if (story.mediaType == 1) { // Image
+            const msg = new webhook.MessageBuilder()
+                .setName("SnapgramLord")
+                .setColor("#aabbcc")
+                .setImage(story.url)
+                .setDescription(
+                    `Timestamp : ${story.timestamp}\n`
+                )
+
+            Hook.send(msg)
+                .then((data) => {
+                    updateData("./metadata-story.json", story.id, true)
+                }).catch((error) => {
+                    console.log("failed to send: " + story.id)
+                })
+        } else if (story.mediaType == 2) {  // Videos
+            
+            const msg = new webhook.MessageBuilder()
+                .setName("SnapgramLord")
+                .setColor("#aabbcc")
+                .setURL(story.url)
+                .setDescription(
+                    `Timestamp : ${story.timestamp}\n`
+                )
+
+            Hook.send(msg)
+                .then((data) => {
+                    updateData("./metadata-story.json", story.id, true)
+                }).catch((error) => {
+                    console.log("failed to send: " + story.id)
+                })
+        }
+    })
